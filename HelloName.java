b6d28344ad6cac522624e7b68a3fc50cd8a32427
Zsuzsa.L <manoaway@gmail.com>
2026-04-12 16:51:03 +0200

    Adhoc lesson 1

---
+++ HelloName.java
@@ -0,0 +1,8 @@
+public class HelloName {    
+    public static void main(String[] args){
+
+String name = args[0];
+String family = args[1];
+
+System.out.println("Hello " + name + " " + family + "!");
+}}
