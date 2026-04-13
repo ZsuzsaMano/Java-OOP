8be2f5e33c8f65b73b351389fb1ee25237fc2a79
Zsuzsa.L <manoaway@gmail.com>
2026-04-13 12:01:10 +0200

    ad fromating

--- Variable.java
+++ Variable.java
@@ -1,10 +1,10 @@
 public class Variable {
-    public static void main(String[] args){
-String str = "I am a string";
-int i = 13;
-boolean bol= true;
-System.out.println(i);
-System.out.println(str);
-System.out.println(bol);
-}
+    public static void main(String[] args) {
+        String str = "I am a string";
+        int i = 13;
+        boolean bol = true;
+        System.out.println(i);
+        System.out.println(str);
+        System.out.println(bol);
+    }
 }
