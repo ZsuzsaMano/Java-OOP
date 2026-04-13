f5de27d4c6159f307d3694bc6243206ee33447a4
Zsuzsa.L <manoaway@gmail.com>
2026-04-13 13:31:25 +0200

    Homework 1.3

---
+++ WunderMax.java
@@ -0,0 +1,18 @@
+// Schreiben Sie eine main-Methode,welche für alle n von 1 bis 100 durch Aufruf der Klassenmethode wunder()den jeweiligen Wert von wunder(n)berechnet 
+// und am Ende ausgibt,welche Zahl die meisten Schritte benötigte und wie viele das waren.
+
+public class WunderMax {
+    public static void main(String[] args) {
+        int max = 0;
+        int nr = 0;
+        for (int i = 1; i <= 100; i++) {
+            int iterations = Wunder.iter(i);
+            if (iterations > max) {
+                max = iterations;
+                nr = i;
+            }
+        }
+        System.out.println("For number " + nr + " : " + max + " iterations");
+    }
+
+}
