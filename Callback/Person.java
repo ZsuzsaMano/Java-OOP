de60a3cb3d9d84d1e2bae5ed44a4c53ae0a5682a
Zsuzsa.L <manoaway@gmail.com>
2026-06-16 21:02:26 +0200

    Create Person  class for  callback

---
+++ Callback/Person.java
@@ -0,0 +1,16 @@
+package Callback;
+
+public class Person {
+    private String name;
+    private double weight;
+
+    public Person(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- Callback/PredicateShortName.java
+++ Callback/PredicateShortName.java
@@ -1,11 +1,9 @@
 package Callback;
 
-import LinkedList.Person;
-
 public class PredicateShortName implements FilterPredicate<Person> {
     @Override
     public boolean isMatching(Person candidate) {
-        return candidate.getFirstName().length() < 5;
+        return candidate.getName().length() < 5;
     }
 
 }
